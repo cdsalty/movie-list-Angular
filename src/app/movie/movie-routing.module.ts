0e17b0307b445627a99cac0d2ecf14372ff29f30
cdsalty <soltiscd@gmail.com>
2019-10-05 15:54:29 -0400

    initial start of application; routes and nav complete

---
+++ src/app/movie/movie-routing.module.ts
@@ -0,0 +1,20 @@
+// Created after setting up app-routing.module
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MovieListComponent,
+  },
+];
+
+@NgModule({
+  imports: [
+      RouterModule.forChild(routes)
+    ],
+  exports: [RouterModule]
+})
+export class MovieRoutingModule { }
